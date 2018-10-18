57e38e439f077bcdd29327918ce602731451c384
Olga Furmanowska <olgafurmanowska@gmail.com>
2018-10-18 15:43:31 +0200

    Update script.js file (add new methods and new function)

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var url = 'https://restcountries.eu/rest/v1/name/';
 var countriesList = $('#countries');
 
 $("#search").click(searchCountries);
+$("#sort").click(sortCountriesList);
 
 // FUNCTION FOR SEARCHING COUNTRIES
 
@@ -21,28 +22,39 @@ function searchCountries() {
 
 function showCountriesList(resp) {
     countriesList.empty();
+    console.log(resp);
     resp.forEach(function(item) {
       var countryData = `
-      <h3>${item.name}</h3>
-      <span class="flag-icon flag-icon-${item.alpha2Code.toLowerCase()}"></span>
-      <ul class="countries_list">
-      <li><span class="countries_list-special">Capital: </span>${item.capital}</li>
-      <li><span class="countries_list-special">Currency: </span>${item.currencies}</li>
-      <li><span class="countries_list-special">Area: </span>${item.area}</li>
-      <li><span class="countries_list-special">Population: </span>${item.population}</li>
-      <li><span class="countries_list-special">Languages: </span>${item.languages}</li>
-      <li><span class="countries_list-special">Region: </span>${item.region}</li>
+      <ul class="countries">
+       <li>
+        <div class="country-name-flag">
+            <span class="flag-icon flag-icon-${item.alpha2Code.toLowerCase()}"></span>
+            <h3>${item.name}</h3>
+        </div>
+        <div class="border"></div>
+          <ul class="countries_list">
+          <li><span class="countries_list-special">Capital: </span>${item.capital}</li>
+          <li><span class="countries_list-special">Currency: </span>${item.currencies}</li>
+          <li><span class="countries_list-special">Area: </span>${item.area}</li>
+          <li><span class="countries_list-special">Population: </span>${item.population}</li>
+          <li><span class="countries_list-special">Languages: </span>${item.languages}</li>
+          <li><span class="countries_list-special">Region: </span>${item.region}</li>
+        </ul>
+        <div class="border"></div>
+       </li>
       </ul>
       `;
 
       $('<li>').html(countryData).appendTo(countriesList);
+      $('.countries').hide("fast");
+      $('.countries').slideDown("slow");
 
 // CHANGING STYLES IN COUNTRIES LIST
 
       $(".countries_list li").
       filter(function( index ) {
         return index % 2 === 0;
-      }).css("color", "#AA3F39");
+      }).css({"color": "#AA3F39", "background-color": "#68979B"});
 
 
       $(".countries_list-special").filter(function( index ) {
@@ -51,4 +63,15 @@ function showCountriesList(resp) {
 
     });
 }
+
+function sortCountriesList(countryData) {
+  //countriesList.empty();
+    var sortedList = countryData.sort(function(a, b) {
+      return a.population > b.population;
+    });
+    console.log(sortedList);
+  $('<li>').html(sortedList).appendTo(countriesList);
+}
+
 searchCountries();
+sortCountriesList()
